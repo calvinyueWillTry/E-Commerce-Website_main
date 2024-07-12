eae2b91e7d6e7dd05e1cfedec1342fc2f16b28b9
Kyler Gibbs <kylergibbs@hotmail.com>
2024-07-11 21:45:14 -0700

    saving work for pull.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,60 @@
+const { User } = require("../models/User");
+const {Product} = require("../models/Product")
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    current: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    createUser: async (parent,{ username, email, password, shippingAddress }) => {
+      const user = await User.create({
+        username,
+        email,
+        password,
+        shippingAddress,
+      });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const correctPass = await user.isCorrectPassword(password);
+      if (!correctPass) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    createProduct: async (parent, {productName, description, image, price, context}) => {
+        const product = await Product.create({ 
+            productName,
+            description,
+            image,
+            price
+        });
+        if (context.user) {
+            const seller = await User.findOneAndUpdate(
+                { _id: context.user._id},
+                {$addToSet: {productsForSale : product}},
+                { new: true}
+            );
+            return {seller}
+        };
+        return { product }
+    }
+},
+};
+
+module.exports = resolvers;
