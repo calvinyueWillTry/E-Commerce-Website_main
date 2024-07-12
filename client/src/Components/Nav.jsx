0f96f221830b5d6244d0b6f79741536d9d65bf4e
calvinyueWillTry <calvin.yue@gmail.com>
2024-07-11 22:01:45 -0700

    revise links

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,28 +1,49 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import "../public/CSS/style.css"
+//need to import css
 
-function Nav ({redirectToThisPage}) {
+function Nav () {
     return (
         <div className="offcanvas-body">
               <ul id="navbar" className="navbar-nav text-uppercase justify-content-end align-items-center flex-grow-1 pe-3">
                 <div>
-                <li className="nav-item">
-                  <a className="nav-link me-4 active" href="#" onClick={()=>redirectToThisPage("Homepage")}>Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link me-4" href="/reviews" onClick={()=>redirectToThisPage("Reviews")}>Reviews</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link me-4" href="/shop" onClick={()=>redirectToThisPage("Shop")}>Let's go Shopping!</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link me-4" href="/signup" onClick={()=>redirectToThisPage("Signup")}>Signup</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link me-4" href="/user" onClick={()=>redirectToThisPage("User")}>User Login</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link me-4" href="/contact" onClick={()=>redirectToThisPage("Contact")}>Contact</a>
-                </li>
+                <div className="nav-item">
+                    <Link className="nav-link" to={"/"}>
+                        <h1> Home </h1>
+                    </Link>
+                </div>
+
+                <div className="nav-item">
+                    <Link className="nav-link" to={"/reviews"}>
+                        <h1> Reviews </h1>
+                    </Link>
+                </div>
+
+                <div className="nav-item">
+                    <Link className="nav-link" to={"/shop"}>
+                        <h1> Let's go Shopping! </h1>
+                    </Link>
+                </div>
+
+                <div className="nav-item">
+                    <Link className="nav-link" to={"/signup"}>
+                        <h1> Signup </h1>
+                    </Link>
+                </div>
+
+                <div className="nav-item">
+                    <Link className="nav-link" to={"/user"}>
+                        <h1> User Login </h1>
+                    </Link>
+                </div>
+
+                <div className="nav-item">
+                    <Link className="nav-link" to={"/contact"}>
+                        <h1> Contact </h1>
+                    </Link>
+                </div>
+
                 </div>
                 
               </ul>
